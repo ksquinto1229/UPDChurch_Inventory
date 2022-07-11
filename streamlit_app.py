29efa84cb270a27ba79cd835dc1474949692fa0f
MellyMel Melnyk <109020141+ksquinto1229@users.noreply.github.com>
2022-07-11 21:54:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+import pandas
+import requests
+from urllib.error import URLError
 
+streamlit.title('My Parents New Health Diner');
+streamlit.header('Breakfast Favorites');
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
