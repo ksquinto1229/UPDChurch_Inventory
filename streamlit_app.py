4ec565a32bba897a30fe141f3a2db5af077a0990
MellyMel Melnyk <109020141+ksquinto1229@users.noreply.github.com>
2022-07-12 00:05:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-#sheet_url = st.secrets["public_gsheets_url"]
-#rows = run_query(f'SELECT * FROM "{sheet_url}"')
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 ### Print results.
-#for row in rows:
+for row in rows:
     st.write(f"{row.name} has a :{row.pet}:")
